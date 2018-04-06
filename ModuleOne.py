a10272ecbb0761cc32078537c9c3a12d926dd3ec
LeslieWilson <leslie.wilson.vt@gmail.com>
2018-04-06 08:06:18 -0400

    cleaned up files

--- ModuleOne.py
+++ ModuleOne.py
@@ -1,13 +1,21 @@
+'''
+ModuleOne.py
+A program for transferring json into a sql database using peewee
+Leslie Wilson, April 2018
+'''
+
 from peewee import *
 import json
 
 db = SqliteDatabase('test.db')
 
+# Basemodel that everything inherits from
+
 class BaseModel(Model):
     class Meta:
         database = db
 
-
+# Create the person and address models
 
 class Person(BaseModel):
     person_id = PrimaryKeyField()
@@ -15,49 +23,44 @@ class Person(BaseModel):
     middlename = CharField(null=True)
     lastname = CharField()
 
-#Why have seperate tables for name and address instead of just a person with name, address, city, zip etc?
 
 class Address(BaseModel):
     address_id = PrimaryKeyField()
     street = CharField()
     city = CharField()
     zipcode = IntegerField()
-    # person = ForeignKeyField(Person, to_field="person_id")
-    # commented out foreign key but necessary to associate tables
-    # create a person table if it does not exist
-
-db.create_tables([Person, Address])
-    # create person instances
-jsondata = json.load(open('input.json'))
-
-for person in jsondata:
-    for address in person['Address']:
-        human = Person(firstname= person['firstname'], middlename = person['middlename'], lastname = person['lastname'])
-        place = Address(street= address['street'], city= address['city'], zipcode= int(address['zipcode']))
-    # save them to database in one transaction
-    # What's the best way to associate an address with the foreign key of this person when saving it in the database?
-
-with db.atomic():
-    human.save()
-    place.save()
+    person = ForeignKeyField(Person, to_field="person_id")
 
 
+def main():
 
-    # # simple SELECT example, select every fields and order by last name. if you don't specify the select columns it selects everything from that model.
-    #
-    # sq = Person.select().order_by(Person.lastname)
-    #
-    # for person in sq:
-    #     print('{} {} {} age {}').format(
-    #     person.firstname, person.middlename, or '\b',
-    #     person.lastname, person.age))
-    #
-    # # example of JOINING conditions:
-    #
-    # # and
-    #
-    # sq.where((Person.firstname == 'Leslie') & (Person.lastname == 'Wilson'))
-    #
-    # # OR
-    #
-    # sq.where((Person.firstname == 'Leslie') | (Person.firstname == 'Julie'))
+    # create a person table and address table if they don't exist
+
+    db.create_tables([Person, Address])
+
+        # create person instances
+
+    jsondata = json.load(open('input.json'))
+
+    for person in jsondata:
+        for address in person['Address']:
+            human = Person(firstname= person['firstname'], middlename = person['middlename'], lastname = person['lastname'])
+            place = Address(street= address['street'], city= address['city'], zipcode= int(address['zipcode']), person= 1)
+        # save them to database in one transaction
+
+    with db.atomic():
+        human.save()
+        place.save()
+
+    print()
+    print("-- get some information and display it")
+
+    sam = Person.get(Person.firstname == 'sam')
+    print("person: {} {}".format(sam.firstname, sam.lastname))
+
+    newyork = Address.get(Address.city == 'new york')
+    print("place: {} {}".format(newyork.street, newyork.city))
+
+    print("a place corresponds to 1 person : place '{}' -> person '{}' ".format(
+              newyork.city, newyork.person.firstname))
+main()
---
+++ readme.md
@@ -0,0 +1,7 @@
+Directions to Run
+===========
+
+* clone the repo
+* make sure peewee and python 3 are installed
+* if they arent, pip install peewee, brew install python
+* browse into the folder and type in your command line:  python ModuleOne.py
